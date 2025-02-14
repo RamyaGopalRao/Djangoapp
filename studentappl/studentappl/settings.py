bd85556e37cb62489b5e216397fbe124fa0c171c
RamyaGopalRao <ramyagopalrao@yahoo.co.in>
2025-02-14 18:03:01 +0530

    Update settings.py

--- studentappl/studentappl/settings.py
+++ studentappl/studentappl/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'studentapp.middleware.EnsureAuthenticatedMiddleware'
 ]
 
 ROOT_URLCONF = 'studentappl.urls'
