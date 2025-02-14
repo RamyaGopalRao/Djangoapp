cccacd1e8158852a6c61858a89e054351cfb087d
RamyaGopalRao <ramyagopalrao@yahoo.co.in>
2025-02-14 17:37:36 +0530

    Update views.py

--- studentappl/studentapp/views.py
+++ studentappl/studentapp/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Student, Course, Marks
 
-class StudentListView(ListView):
+class StudentListView(LoginRequiredMixin,ListView):
     model = Student
     template_name = 'student_list.html'
 
-class StudentDetailView(DetailView):
+class StudentDetailView(LoginRequiredMixin,DetailView):
     model = Student
     template_name = 'studentapp/student_detail.html'
 
-class StudentCreateView(CreateView):
+class StudentCreateView(LoginRequiredMixin,CreateView):
     model = Student
     template_name = 'studentapp/student_form.html'
     fields = ['name', 'age', 'grade', 'address', 'course']
@@ -22,7 +23,7 @@ class StudentCreateView(CreateView):
         form.instance.course.set(self.request.POST.getlist('course'))
         return response
 
-class StudentUpdateView(UpdateView):
+class StudentUpdateView(LoginRequiredMixin,UpdateView):
     model = Student
     template_name = 'studentapp/student_form.html'
     fields = ['name', 'age', 'grade', 'address', 'course']
@@ -33,31 +34,31 @@ class StudentUpdateView(UpdateView):
         form.instance.course.set(self.request.POST.getlist('course'))
         return response
 
-class StudentDeleteView(DeleteView):
+class StudentDeleteView(LoginRequiredMixin,DeleteView):
     model = Student
     template_name = 'student_confirm_delete.html'
     success_url = reverse_lazy('student_list')
-class CourseListView(ListView):
+class CourseListView(LoginRequiredMixin,ListView):
     model = Course
     template_name = 'studentapp/course_list.html'
 
-class CourseDetailView(DetailView):
+class CourseDetailView(LoginRequiredMixin,DetailView):
     model = Course
     template_name = 'studentapp/course_detail.html'
 
-class CourseCreateView(CreateView):
+class CourseCreateView(LoginRequiredMixin,CreateView):
     model = Course
     template_name = 'studentapp/course_form.html'
     fields = ['name']
     success_url = reverse_lazy('course_list')
 
-class CourseUpdateView(UpdateView):
+class CourseUpdateView(LoginRequiredMixin,UpdateView):
     model = Course
     template_name = 'studentapp/course_form.html'
     fields = ['name']
     success_url = reverse_lazy('course_list')
 
-class CourseDeleteView(DeleteView):
+class CourseDeleteView(LoginRequiredMixin,DeleteView):
     model = Course
     template_name = 'studentapp/course_confirm_delete.html'
     success_url = reverse_lazy('course_list')
